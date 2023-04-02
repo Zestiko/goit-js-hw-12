6d68fd9dbbdb362cab5cf95f54cc86567c4c45cd
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2023-04-02 15:06:32 +0300

    add vimeo dependencies

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
-console.log('hello')
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+console.log(iframe)
+
+
